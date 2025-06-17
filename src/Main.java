11f195e2ffeb07b9442d0c40acffaf8751d9d80f
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-17 10:27:18 +0900

    Lambda/Method reference

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-// 람다식은 하나의 추상 메소드가 있는 인터페이스만
-@FunctionalInterface
-interface MathOperation{
-    int oprate(int a, int b);
-
-    // 에러. 하나의 추상 메소드만
-//    int extraMethod();
-}
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        MathOperation add = (a, b) -> a + b;
-        MathOperation mul = (a, b) -> a * b;
+        String[] names = {"Alice", "Bob", "Charlie"};
 
-        System.out.println(add.oprate(3,4) + ", " + mul.oprate(4,6));
+        // 람다
+        Arrays.stream(names).forEach(name -> System.out.println(name));
+
+        System.out.println();
+
+        // 메소드 참조
+        Arrays.stream(names).forEach(System.out::println);
     }
 }
